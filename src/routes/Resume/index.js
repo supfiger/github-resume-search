7ea7cf26d0ffa11165219d3c4a0233bcc84fd623
Danil Zinchenko <danilzin777@gmail.com>
2021-07-20 14:56:07 +0300

    added layout to Resume page

--- src/routes/Resume/index.js
+++ src/routes/Resume/index.js
@@ -1,3 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container, Row, Table } from "reactstrap";
+
+function Header() {
+  return (
+    <>
+      <Row className="border-bottom py-3">
+        <Link to="/">← Back to home</Link>
+      </Row>
+      <Row className="border-bottom py-3">
+        <h1>{"John John"} — Resume</h1>
+        <h2 className="h4">GitHub profile</h2>
+      </Row>
+    </>
+  );
+}
+
+function UserInfo() {
+  return (
+    <Row>
+      <Table>
+        <tbody>
+          <tr>
+            <th scope="row">{}</th>
+            <td>{}</td>
+          </tr>
+        </tbody>
+      </Table>
+    </Row>
+  );
+}
+
 export default function Resume() {
-  return <div>Resume</div>;
+  return (
+    <Container>
+      <Header />
+      <UserInfo />
+    </Container>
+  );
 }
