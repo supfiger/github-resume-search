e227890cf536ca9aaba373785fe824b069fa3d65
Danil Zinchenko <danilzin777@gmail.com>
2021-07-20 11:08:13 +0300

    added home page layout

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,3 +1,43 @@
+import React from "react";
+import {
+  InputGroup,
+  InputGroupAddon,
+  Input,
+  Button,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Row>
+        <h1>Home</h1>
+      </Row>
+      <hr />
+      <Row>
+        <h2 className="h4">Search for information about any github user</h2>
+      </Row>
+      <hr />
+      <Row>
+        <p>
+          This application can show you an information about any github user.{" "}
+          <br />
+          Just enter a username you want and click the "Search" button.
+        </p>
+      </Row>
+      <hr />
+      <Row>
+        <Col sm className="py-2">
+          <InputGroup>
+            <Input placeholder="Enter a username" />
+            <InputGroupAddon addonType="append">
+              <Button>Search</Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
