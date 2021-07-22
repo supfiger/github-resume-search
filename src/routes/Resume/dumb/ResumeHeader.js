8ce33d4c53d550ee24eed2e5272dac7b19c4b361
Danil Zinchenko <danilzin777@gmail.com>
2021-07-22 07:10:30 +0300

    Updates

--- src/routes/Resume/dumb/ResumeHeader.js
+++
@@ -1,8 +0,0 @@
-import Header from "../../../components/Header";
-
-export default function ResumeHeader({ userData }) {
-  const { name } = userData;
-  const title = name ? `${name}  — GitHub resume` : `GitHub resume`;
-
-  return <Header back title={title}></Header>;
-}
